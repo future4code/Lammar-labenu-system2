b04f57de3276701f348056a2ea0e455ec504a150
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-12-18 15:59:25 -0300

    Criação da database

---
+++ src/models/Estudante.ts
@@ -0,0 +1,21 @@
+import { Usuario } from "./Usuario"
+
+export class Estudante extends Usuario {
+    constructor(
+        id: string,
+        nome: string,
+        email: string,
+        data_nasc: Date,
+        turma_id: string,
+        private hobbies: string[]
+    ) {
+        super(
+            id,
+            nome,
+            email,
+            data_nasc,
+            turma_id
+        )
+        this.hobbies = hobbies
+    }
+}
